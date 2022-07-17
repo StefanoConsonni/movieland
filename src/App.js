5f19b67c5ba30bf969e564816afa0fc0c8f445a5
Stefano Consonni <stefanoconsonni@ymail.com>
2022-07-17 15:57:27 +0200

    add search bar and css styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 
+import "./App.css";
+import SearchIcon from "./search.svg";
+
 // Here is your movie db key: 175fe5b1
 const API_URL = "http://www.omdbapi.com/?apikey=175fe5b1";
 
@@ -14,7 +17,15 @@ const App = () => {
 		searchMovies("Batman");
 	}, []);
 
-	return <h1>App</h1>;
+	return (
+		<div className="app">
+			<h1>MovieLand</h1>;
+			<div className="search">
+				<input placeholder="Search for movies" value="Superman" type="text" onChange={() => {}} />
+				<img src={SearchIcon} alt="search icon" onClick={() => {}} />
+			</div>
+		</div>
+	);
 };
 
 export default App;
