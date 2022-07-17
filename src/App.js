e81061096e0c0cf4e78240de5f030118cac28413
Stefano Consonni <stefanoconsonni@ymail.com>
2022-07-17 19:07:07 +0200

    create MovieCard component and import it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { useEffect } from "react";
 
+import MovieCard from "./MovieCard";
+
 import "./App.css";
 import SearchIcon from "./search.svg";
 
 // Here is your movie db key: 175fe5b1
-const API_URL = "http://www.omdbapi.com/?apikey=175fe5b1";
+const API_URL = "https://www.omdbapi.com/?apikey=175fe5b1";
+
+const movie1 = {
+	Title: "Batman Begins",
+	Year: "2005",
+	imdbID: "tt0372784",
+	Type: "movie",
+	Poster: "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
 
 const App = () => {
 	const searchMovies = async (title) => {
@@ -23,6 +33,9 @@ const App = () => {
 			<div className="search">
 				<input placeholder="Search for movies" value="Superman" type="text" onChange={() => {}} />
 				<img src={SearchIcon} alt="search icon" onClick={() => {}} />
+			</div>
+			<div className="container">
+				<MovieCard movie1={movie1} />
 			</div>
 		</div>
 	);
